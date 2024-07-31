7f2a0fc48589d52c9dda9a7cf36e8997f5a0fbaf
Gustavo Marin <wstov@hotmail.com>
2024-07-30 19:08:12 -0600

    Conexión a la base de datos
    conexión con php a la DB

---
+++ php/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$database = "BookshopDB";
+
+$conn = new mysqli($servername, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+echo "Conexión exitosa";
+
+?>
